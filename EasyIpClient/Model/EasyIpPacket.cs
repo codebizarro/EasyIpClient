c60aac448334d671ee255b79a7cf4315233021a4
Rudenko.A.E <Rudenko.A.E.@localhost>
2017-06-20 14:36:07 +0200

    Added class documentation, updated fields data types accordings to new requirements

--- EasyIpClient/Model/EasyIpPacket.cs
+++ EasyIpClient/Model/EasyIpPacket.cs
@@ -2,19 +2,85 @@
 {
     public class EasyIpPacket
     {
-        public char Flags;
-        public char Error;
+        /// <summary>
+        /// 1 byte
+        /// Bit 0 information packet (request or response)
+        /// Bit 1-2 bit operations 1
+        /// Bit 6 do not respond
+        /// Bit 7 response packet
+        /// </summary>
+        public byte Flags;
+        /// <summary>
+        /// 1 byte
+        /// Only used in response packets
+        /// 0: no error
+        /// 1: operand type error
+        /// 2: offset error
+        /// 4: size error
+        /// 16: no support
+        /// </summary>
+        public byte Error;
+        /// <summary>
+        /// 4 bytes
+        /// Set by client, copied by server
+        /// </summary>
         public short Counter;
+        /// <summary>
+        /// 1 byte
+        /// Reserved
+        /// Set to 0
+        /// </summary>
         public short ShortIndex;
-        public char Spare1;
-        public char SendDataType;
+        /// <summary>
+        /// ???
+        /// </summary>
+        public byte Spare1;
+        /// <summary>
+        /// 1 byte
+        /// Type of operand, some types may not be available
+        /// 1 memory word
+        /// 2 input word
+        /// 3 output word
+        /// 4 register
+        /// 5 timer
+        /// 11 strings3
+        /// </summary>
+        public byte SendDataType;
+        /// <summary>
+        /// 2 bytes
+        /// Number of words
+        /// </summary>
         public short SendDataSize;
+        /// <summary>
+        /// 2 bytes
+        /// Target offset in server
+        /// </summary>
         public short SendDataOffset;
-        public char Spare2;
-        public char ReqDataType;
+        /// <summary>
+        /// 1 byte
+        /// Reserved
+        /// Set to 0
+        /// </summary>
+        public byte Spare2;
+        /// <summary>
+        /// 1 byte
+        /// Type of operand, some types may not be available (see senddata type for list of types)
+        /// </summary>
+        public byte ReqDataType;
+        /// <summary>
+        /// 2 bytes
+        /// Number of words
+        /// </summary>
         public short ReqDataSize;
+        /// <summary>
+        /// 2 bytes
+        /// Offset in server
+        /// </summary>
         public short ReqDataOffsetServer;
+        /// <summary>
+        /// 2 bytes
+        /// Target offset in client
+        /// </summary>
         public short ReqDataOffsetClient;
-        public short[] Data = new short[256];
     }
 }
