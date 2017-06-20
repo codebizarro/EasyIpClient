8c6c309afa014fb30884fcd37c5350ec00ef1a16
Rudenko.A.E. <Rudenko.A.E.@localhost>
2017-06-20 10:36:00 +0200

    Added EasyIpPacket definition

---
+++ EasyIpClient/Model/EasyIpPacket.cs
@@ -0,0 +1,20 @@
+﻿namespace EasyIpClient.Model
+{
+    public class EasyIpPacket
+    {
+        public char Flags;
+        public char Error;
+        public short Counter;
+        public short ShortIndex;
+        public char Spare1;
+        public char SendDataType;
+        public short SendDataSize;
+        public short SendDataOffset;
+        public char Spare2;
+        public char ReqDataType;
+        public short ReqDataSize;
+        public short ReqDataOffsetServer;
+        public short ReqDataOffsetClient;
+        public short[] Data = new short[256];
+    }
+}
