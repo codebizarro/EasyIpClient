c7fea7180ab19f94893b8d380959ab6e4b250058
Rudenko.A.E. <Rudenko.A.E.@localhost>
2017-06-20 09:32:36 +0200

    Created base channel classes

---
+++ EasyIpClient/Channel/TcpChannel.cs
@@ -0,0 +1,99 @@
+﻿using EasyIpClient.Channel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EasyIpClient.Channel
+{
+    internal class TcpChannel: IChannel, IDisposable
+    {
+        private TcpClient Client;
+        private NetworkStream stream;
+
+        public TcpChannel(IPEndPoint endpoint)
+        {
+            Client = new TcpClient();
+            Client.Connect(endpoint);
+            stream = Client.GetStream();
+            if (!stream.CanWrite) throw new Exception("Stream don't ready to write");
+        }
+
+        public byte[] Execute(byte[] buffer)
+        {
+            stream.Write(buffer, 0, buffer.Length);
+            List<byte> lst = new List<byte>();
+            if (stream.CanRead)
+            {
+                byte[] buff = new byte[1024];
+                int n = 0;
+                do
+                {
+                    n = stream.Read(buff, 0, buff.Length);
+                    for (int i = 0; i < n; ++i)
+                        lst.Add(buff[i]);
+                }
+                while (stream.DataAvailable);
+            }
+            return lst.ToArray();
+        }
+
+        public int SendTimeout
+        {
+            get
+            {
+                return Client.Client.SendTimeout;
+            }
+            set
+            {
+                Client.Client.SendTimeout = value;
+            }
+        }
+
+        public int ReceiveTimeout
+        {
+            get
+            {
+                return Client.Client.ReceiveTimeout;
+            }
+            set
+            {
+                Client.Client.ReceiveTimeout = value;
+            }
+        }
+
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    if (stream != null)
+                    {
+                        stream.Close();
+                        stream = null;
+                    }
+                    if (Client != null)
+                    {
+                        Client.Close();
+                        Client = null;
+                    }
+                }
+                disposed = true;
+            }
+        }
+
+        ~TcpChannel()
+        {
+            Dispose(false);
+        }
+    }
+}
