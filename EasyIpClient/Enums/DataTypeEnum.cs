1b1fd1b6975de009f691d03a6ecfb236dcad6f58
Rudenko.A.E. <Rudenko.A.E.@localhost>
2017-06-20 09:48:46 +0200

    Added EasyIp enums and constants

---
+++ EasyIpClient/Enums/DataTypeEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyIpClient.Enums
+{
+    public enum DataTypeEnum
+    {
+        FlagWord = 1,
+        InputWord = 2,
+        OutputWord = 3,
+        Regitsters = 4,
+        Strings = 11
+    }
+}
---
+++ EasyIpClient/Enums/ErrorTypeEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyIpClient.Enums
+{
+    public enum ErrorTypeEnum
+    {
+        Operand = 0x01,
+        Offset = 0x02,
+        Datasize = 0x04,
+        Nosupport = 0x10
+    }
+}
