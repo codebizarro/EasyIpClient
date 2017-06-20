6c9e5a64b9a46459d0a016f046ed3dad11b6db01
Rudenko.A.E. <Rudenko.A.E.@localhost>
2017-06-20 15:29:12 +0200

    Added skeleton of execute test

--- EasyIpClientTest/ChannelTest.cs
+++ EasyIpClientTest/ChannelTest.cs
@@ -3,6 +3,9 @@ using System.Net;
 using EasyIpClient.Channel.Interfaces;
 using EasyIpClient.Channel;
 using EasyIpClient.Constants;
+using EasyIpClient.Model;
+using EasyIpClient.Enums;
+using System;
 
 namespace EasyIpClientTest
 {
@@ -15,6 +18,28 @@ namespace EasyIpClientTest
             var client = new UdpChannel(new IPEndPoint(IPAddress.Parse(Configuration.Address), Constants.EASYIP_PORT));
             Assert.IsNotNull(client);
             Assert.IsInstanceOfType(client, typeof(IChannel));
+        }
+
+        [TestMethod]
+        public void ExecuteReadTest()
+        {
+            IChannel client = new UdpChannel(new IPEndPoint(IPAddress.Parse(Configuration.Address), Constants.EASYIP_PORT));
+            var packet = new EasyIpPacket
+            {
+                Flags = 0,
+                Error = 0,
+                Counter = 0, // Must increment in client
+                SendDataType = 0,
+                SendDataSize = 0,
+                SendDataOffset = 0,
+                ReqDataType = DataTypeEnum.FlagWord,
+                ReqDataSize = 20,
+                ReqDataOffsetServer = 0,
+                ReqDataOffsetClient = 0
+            };
+            
+            //client.Execute()
+            
         }
     }
 }
