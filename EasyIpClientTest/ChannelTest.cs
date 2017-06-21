5baffdb69be0c37055dbef2944bc5d5780f9955b
Rudenko.A.E. <Rudenko.A.E.@localhost>
2017-06-21 19:33:23 +0200

    Improved serialization method of request packet

--- EasyIpClientTest/ChannelTest.cs
+++ EasyIpClientTest/ChannelTest.cs
@@ -83,5 +83,55 @@ namespace EasyIpClientTest
             Assert.IsNotNull(response);
             Assert.IsTrue(response[1] == 0);
         }
+
+        //[TestMethod]
+        public void ExecuteReadWriteBenchmarkTest()
+        {
+            IChannel client = new UdpChannel(new IPEndPoint(IPAddress.Parse(Configuration.Address), Constants.EASYIP_PORT));
+            var writePacket = new EasyIpPacket
+            {
+                Flags = 0,
+                Error = 0,
+                Counter = 0, // Must increment in client
+                SendDataSize = 2,
+                SendDataOffset = 0,
+                SendDataType = DataTypeEnum.FlagWord,
+                ReqDataSize = 0,
+                ReqDataOffsetServer = 0,
+                ReqDataOffsetClient = 0
+            };
+            var readPacket = new EasyIpPacket
+            {
+                Flags = 0,
+                Error = 0,
+                Counter = 0, // Must increment in client
+                SendDataType = 0,
+                SendDataSize = 0,
+                SendDataOffset = 0,
+                ReqDataType = DataTypeEnum.FlagWord,
+                ReqDataSize = 20,
+                ReqDataOffsetServer = 0,
+                ReqDataOffsetClient = 0
+            };
+
+            for (int i = 0; i < ushort.MaxValue; i++)
+            {
+                writePacket.Data[0] = (short)i;
+                var response = client.Execute(writePacket.BuildRequest());
+                Assert.IsNotNull(response);
+                Assert.IsTrue(response[1] == 0);
+
+                response = client.Execute(readPacket.BuildRequest());
+                Assert.IsNotNull(response);
+                Assert.IsTrue(response[1] == 0);
+                Assert.AreEqual((byte)readPacket.ReqDataType, response[13]);
+                Assert.AreEqual(readPacket.ReqDataSize, response[14]);
+
+                ushort testValue = response[21];
+                testValue <<= 8;
+                testValue |= response[20];
+                Assert.AreEqual(i, testValue);
+            }
+        }
     }
 }
