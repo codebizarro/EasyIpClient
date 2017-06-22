f522d8c7724634ba256c2c24e37922cd69ef669f
Rudenko.A.E. <Rudenko.A.E.@localhost>
2017-06-22 09:27:51 +0200

    Improved organization of unit tests

--- EasyIpClientTest/ChannelTest.cs
+++ EasyIpClientTest/ChannelTest.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Net;
 using EasyIpClient.Channel.Interfaces;
 using EasyIpClient.Channel;
 using EasyIpClient.Constants;
 using EasyIpClient.Model;
 using EasyIpClient.Enums;
-using System;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
 
 namespace EasyIpClientTest
 {
@@ -20,19 +16,6 @@ namespace EasyIpClientTest
             var client = new UdpChannel(Configuration.Address, Constants.EASYIP_PORT);
             Assert.IsNotNull(client);
             Assert.IsInstanceOfType(client, typeof(IChannel));
-        }
-
-        byte[] ObjectToByteArray(object obj)
-        {
-            if (obj == null)
-                return null;
-            var formatter = new BinaryFormatter();
-            using (var stream = new MemoryStream())
-            {
-                formatter.Serialize(stream, obj);
-                var ret = stream.ToArray();
-                return ret;
-            }
         }
 
         [TestMethod]
@@ -82,56 +65,6 @@ namespace EasyIpClientTest
             var response = client.Execute(packet.BuildRequest());
             Assert.IsNotNull(response);
             Assert.IsTrue(response[1] == 0);
-        }
-
-        //[TestMethod]
-        public void ExecuteReadWriteBenchmarkTest()
-        {
-            IChannel client = new UdpChannel(new IPEndPoint(IPAddress.Parse(Configuration.Address), Constants.EASYIP_PORT));
-            var writePacket = new EasyIpPacket
-            {
-                Flags = 0,
-                Error = 0,
-                Counter = 0, // Must increment in client
-                SendDataSize = 2,
-                SendDataOffset = 0,
-                SendDataType = DataTypeEnum.FlagWord,
-                ReqDataSize = 0,
-                ReqDataOffsetServer = 0,
-                ReqDataOffsetClient = 0
-            };
-            var readPacket = new EasyIpPacket
-            {
-                Flags = 0,
-                Error = 0,
-                Counter = 0, // Must increment in client
-                SendDataType = 0,
-                SendDataSize = 0,
-                SendDataOffset = 0,
-                ReqDataType = DataTypeEnum.FlagWord,
-                ReqDataSize = 20,
-                ReqDataOffsetServer = 0,
-                ReqDataOffsetClient = 0
-            };
-
-            for (int i = 0; i < 1000 /*ushort.MaxValue*/; i++)
-            {
-                writePacket.Data[0] = (short)i;
-                var response = client.Execute(writePacket.BuildRequest());
-                Assert.IsNotNull(response);
-                Assert.IsTrue(response[1] == 0);
-
-                response = client.Execute(readPacket.BuildRequest());
-                Assert.IsNotNull(response);
-                Assert.IsTrue(response[1] == 0);
-                Assert.AreEqual((byte)readPacket.ReqDataType, response[13]);
-                Assert.AreEqual(readPacket.ReqDataSize, response[14]);
-
-                ushort testValue = response[21];
-                testValue <<= 8;
-                testValue |= response[20];
-                Assert.AreEqual(i, testValue);
-            }
         }
     }
 }
