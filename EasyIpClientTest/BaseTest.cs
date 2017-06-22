a591a10dc1d79ac448a1e841418da5fc7c2bfe1e
Rudenko.A.E. <Rudenko.A.E.@localhost>
2017-06-22 09:39:45 +0200

    Moved some values to constants, Created base test class

---
+++ EasyIpClientTest/BaseTest.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyIpClientTest
+{
+    public class BaseTest
+    {
+        protected const int BENCHMARK_COUNT = 1000;
+        protected const short SEND_DATA_SIZE = 200;
+        protected const short RECEIVE_DATA_SIZE = 200;
+    }
+}
