cfda8e2cdb0393e104490470f50b0c4b787d0949
Rudenko.A.E <Rudenko.A.E.@localhost>
2017-06-23 09:34:02 +0200

    Applied dispose pattern with common base class

---
+++ EasyIpClient/Common/Disposable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EasyIpClient.Common
+{
+    public abstract class Disposable : IDisposable
+    {
+        // Flag: Has Dispose already been called?
+        private bool _disposed;
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+            _disposed = true;
+        }
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+    }
+}
